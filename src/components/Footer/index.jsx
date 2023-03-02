b6fa31fb18a516addb5103d793174b900f655090
vichnyi-myzuka <62535515+vichnyi-myzuka@users.noreply.github.com>
2023-03-02 11:11:01 +0200

    fix: update year

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Факультет Інформатики, 2022</p>
+      <p>Факультет Інформатики, 2023</p>
     </footer>
   );
 }
